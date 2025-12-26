f3bc126a94fb0111e837ff529fdd7936e029bbc7
taaeee <osvaldo106@hotmail.com>
2025-12-25 18:35:48 -0600

    more steamid

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,11 +1,6 @@
 import { LoginButton } from "./ui/LoginButton";
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+import { supabase } from "@/services/supabaseClient";
 
 export function Login() {
   const [session, setSession] = useState(null);
