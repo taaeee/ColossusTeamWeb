36e47bed93791ecfaa4aa60d8b3a6cf4947ca085
taaeee <osvaldo106@hotmail.com>
2025-12-30 02:10:59 -0600

    test 2

--- components/BetView.tsx
+++ components/BetView.tsx
@@ -1020,7 +1020,7 @@ export const BetView: React.FC<BetViewProps> = () => {
       }
 
       // Si completamos todos los picks, transicionar a READY
-      if (pickedCount >= 7) {
+      if (pickedCount >= 1) {
         transitionToReady();
       }
     } catch (error) {
